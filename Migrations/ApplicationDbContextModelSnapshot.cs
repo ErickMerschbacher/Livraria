39e1225f59527ad43b0a6327c9be0d2e01b81639
manuela582279 <manuela.domingues@aluno.senai.br>
2026-02-05 14:58:41 -0300

    Reestruturação do banco e visual, Identity customizado
    - Removidas migrações antigas e snapshot antigo do banco
    - Criada nova migração unificada com estrutura completa (Identity, Autores, Clientes, Livros, Empréstimos)
    - Adicionadas páginas de autenticação Identity customizadas e estilizadas em português
    - Atualizado _ViewImports.cshtml e _ValidationScriptsPartial.cshtml para suporte ao Identity e validação
    - Adicionado pacote Microsoft.EntityFrameworkCore.Sqlite
    - Reformuladas visualmente as views de CRUD (Create/Index) para todos os módulos, com design moderno e usabilidade aprimorada
    - Corrigido pequeno erro de sintaxe em site.css
    - Sistema pronto para uso com autenticação e visual padronizado

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,19 +4,16 @@ using Livraria.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Livraria.Data.Migrations
+namespace Livraria.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260205125529_novaaa")]
-    partial class novaaa
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
